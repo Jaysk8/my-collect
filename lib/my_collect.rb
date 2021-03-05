167a002f2010b173f207f0aa8d08c46321dd184c
Jonathan <ruizjayy@icloud.com>
2021-03-05 03:07:07 -0600

    commit tree 178b396f65a411bbc3ac3f33b1910b48c9d12111

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+require 'pry'
+def my_collect(arg_array)
+    collection = []
+    i = 0
+    while i < arg_array.length
+      collection << yield(arg_array[i])
+      i += 1
+    end
+    collection
+end
 
-
+# collection = []
+ #    collection << languages
+  # i = 0
+  # while i < arr.length
+  #  yield(arr[i])
+  #  i += 1
